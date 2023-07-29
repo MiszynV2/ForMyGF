75a689636fefc4ccc3042f39a7f59a910afe4a13
Miszyn <wmarciniszyn11@gmail.com>
2023-07-29 20:39:52 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import FolderItem from "./components/FolderItem";
 import FolderList from "./components/FolderList";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app-wrapper">
       <FolderList />
+      <Footer />
     </div>
   );
 }
