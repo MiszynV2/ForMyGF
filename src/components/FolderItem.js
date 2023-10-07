cf6a46b8e9dfaacfb4307792be8196f5b07e1aee
Miszyn <wmarciniszyn11@gmail.com>
2023-10-07 23:39:20 +0200

    add: dragging element, doubleclick on open, Highlighted on click

--- src/components/FolderItem.js
+++ src/components/FolderItem.js
@@ -1,11 +1,55 @@
 import React, { useState } from "react";
-
 import classes from "./FolderItem.module.css";
 
 function FolderItem({ onClick, name, alt, icon }) {
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
+  const [offsetX, setOffsetX] = useState(0);
+  const [offsetY, setOffsetY] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [isHighlighted, setIsHighlighted] = useState(false);
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.clientX - offsetX);
+    setStartY(e.clientY - offsetY);
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      const newOffsetX = e.clientX - startX;
+      const newOffsetY = e.clientY - startY;
+      setOffsetX(newOffsetX);
+      setOffsetY(newOffsetY);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    setIsHighlighted(true);
+    setTimeout(() => {
+      setIsHighlighted(false);
+    }, 760);
+  };
+
+  const handleClick = () => {
+    setIsDragging(false);
+  };
+
   return (
-    <div className={classes.FolderItemWrapper} onClick={onClick}>
-      {/* Alt is an empty string as the icon does not add any additional value over name of the folder */}
+    <div
+      className={`${classes.FolderItemWrapper} ${
+        isHighlighted ? classes. : ""
+      }`}
+      style={{
+        transform: `translate(${offsetX}px, ${offsetY}px)`,
+      }}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onDoubleClick={onClick}
+      onClick={handleClick}
+    >
       <img src={icon} className={classes.FolderLogo} alt={""} />
       <h1 className={classes.FolderText}>{name}</h1>
     </div>
