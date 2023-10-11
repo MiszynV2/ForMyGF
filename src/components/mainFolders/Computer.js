b9391597c563bacd612a59c99e9a8a6453844565
Miszyn <wmarciniszyn11@gmail.com>
2023-10-11 20:41:17 +0200

    added better mycomputer css

--- src/components/mainFolders/Computer.js
+++ src/components/mainFolders/Computer.js
@@ -13,18 +13,17 @@ function Computer({ close }) {
       <TitleBar image={image} title={title} close={close} />
       <div className={classes.Wrapper}>
         <Options options={options} />
+        <div className={classes.SectionWrapper}>
+          <div className={classes.NavSection}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
 
-        <textarea className={classes.Text} spellCheck="false">
-          As a Junior Front-End Engineer, I am passionate about creating
-          engaging user experiences and building responsive web applications.
-          With a strong foundation in HTML, CSS, and JavaScript, I am
-          experienced in creating visually appealing and user-friendly websites.
-          I am also familiar with various front-end frameworks such as React and
-          enjoy collaborating with other developers, designers, and stakeholders
-          to create high-quality end products. Outside of work, I stay
-          up-to-date with the latest industry news and trends and enjoy making
-          new recipes in my kitchen.
-        </textarea>
+          <div className={classes.RightSection}>
+            <div></div>
+          </div>
+        </div>
       </div>
     </Window>
   );
